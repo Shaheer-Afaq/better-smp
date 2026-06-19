2a73bfa1d0a7fbb648ae244052b0140cc39df126
Shaheer-Afaq <23121997doa@gmail.com>
2026-06-19 22:26:13 +0500

    added get_wood quest which gives xp and wooden tools

---
+++ src/main/java/better_smp/PlayerStats.java
@@ -0,0 +1,12 @@
+package better_smp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerStats {
+    public enum achievements
+    {
+        GET_WOOD,
+        FIRST_KILL
+    }
+}
---
+++ src/main/resources/data/better_smp/function/rewards/wood_quest.mcfunction
@@ -0,0 +1,1 @@
+give @s wooden_axe 1
